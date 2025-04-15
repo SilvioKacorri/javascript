afcdbb0ae01e7490e375e4389008983b72a08237
SilvioKacorri <s.kacorri@calmcoders.net>
2025-04-15 17:53:37 +0200

    3 parts working calculator

--- calc.js
+++ calc.js
@@ -1,19 +1,99 @@
-let value = 0;
+
+let value = null;
+
+let part1 = '';
+let part2 = '';
+let part3 = '';
 
 var display = document.getElementById("number-display");
 
-var buttons = document.querySelectorAll('button')
+var buttons = document.querySelectorAll('.number')
+
+var specialButtons = document.querySelectorAll('.special')
 
 
 function setValue (button) {
     value = button.value;
-    console.log(value);
+    if (part2 == '') {
+        part1 = part1.concat(value);
+    }
+    else{
+        part3 = part3.concat(value)
+    }
+    writeDisplay();
 };
 
 buttons.forEach(button => {
     button.addEventListener('click', () => setValue(button));
 });
 
-console.log(value);
+const clear = document.getElementById('clear');
+    clear.addEventListener('click' , () => {
+        part1 = '';
+        part2 = '';
+        part3 = '';
+        display.textContent = '';
+});
 
-display.textContent = value;
+specialButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        part2 = button.textContent;
+        console.log(part2)
+    })
+})
+
+function clearAfterResult(){
+    part1 = '';
+    part2 = '';
+    part3 = '';
+    display.textContent = null;
+}
+
+const test = document.getElementById('test')
+test.addEventListener('click', () => {
+    console.log("part 1 " + part1);
+    console.log("part 2 " + part2);
+    console.log("part 3 " + part3); 
+
+});
+
+
+const calculate = document.getElementById('equals');
+calculate.addEventListener('click', () => {
+    let result = 0;
+    part1 = parseInt(part1);
+    part3 = parseInt(part3);
+    switch(part2){
+        case "+":
+            result = part1 + part3;
+            console.log(parseInt(result));
+            break;
+        case "/":
+            result = part1 / part3;
+            break;
+        case "*":
+            result = part1 * part3;
+            break;
+        case "-":
+            result = part1 - part3;
+            break;
+        case "%":
+            result = part1 % part3;
+            break;
+    }
+    clearAfterResult();
+
+
+    display.textContent = result;
+})
+
+function writeDisplay(){
+    display.textContent = part1 + " " + part2 + " " + part3;
+}
+
+
+
+
+
+
+
